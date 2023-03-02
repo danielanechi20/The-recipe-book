b4c65fb5794d3ccea58d09fdc2066452559117c1
danielanechi20 <daniela_nechifor33@yahoo.com>
2023-03-02 16:11:26 +0200

    babysteps

--- app/src/main/java/com/example/licentaincercarea1/MainActivity.kt
+++ app/src/main/java/com/example/licentaincercarea1/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.licentaincercarea1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
