51ab3221dc1e71384d3e1022991f82fa0961fae5
danielanechi20 <daniela_nechifor33@yahoo.com>
2023-03-30 21:03:32 +0300

    corect

--- app/src/main/java/com/example/licentaincercarea1/CategoriesFragment.kt
+++ app/src/main/java/com/example/licentaincercarea1/CategoriesFragment.kt
@@ -1,11 +1,17 @@
 package com.example.licentaincercarea1
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.createNavigateOnClickListener
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.licentaincercarea1.databinding.CategoriesFragmentBinding
@@ -20,43 +26,48 @@ class CategoriesFragment : Fragment() {
     private var _binding: CategoriesFragmentBinding?=null
     private val binding get()=_binding!!
 
-    var categories = ArrayList<String>()
-    var desc = ArrayList<String>()
-    var image = ArrayList<String>()
+    private var categories = ArrayList<String>()
+    private var desc = ArrayList<String>()
+    private var image = ArrayList<String>()
     private val categoriesAdapter = CategoriesAdapter(categories,desc,image)
 
-    var retete = ArrayList<String>()
-    var ingrediente = ArrayList<String>()
-    var images = ArrayList<String>()
-    var pasi=ArrayList<String>()
-    private val vitaAdapter=ReteteAdapter(retete,ingrediente,images,pasi)
+    private var retete = ArrayList<String>()
+    private var ingrediente = ArrayList<String>()
+    private var images = ArrayList<String>()
+    private var pasi=ArrayList<String>()
+
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         _binding = CategoriesFragmentBinding.inflate(inflater,container,false)
         val view = binding.root
         setupRv()
-        transfcat(CATEGORIES)
-        transfretete(VITA,"vita")
+        transfcat()
         return view
     }
 
-    private fun transfcat(fileName: String){
+    private fun transfcat(){
         // get JSONObject from JSON file
-        val obj = JSONObject(loadJSONFromAsset(fileName))
+        val obj = JSONObject(loadJSONFromAsset(CATEGORIES))
         // fetch JSONArray named colors
         val userArray: JSONArray = obj.getJSONArray("categories")
         // implement a loop to get colors list data
         for (i in 0 until userArray.length()) {
             // create a JSONObject for fetching single color data
             val userDetail = userArray.getJSONObject(i)
-            // fetch color name and category and store it in arraylist
             categories.add(userDetail.getString("Name"))
             desc.add(userDetail.getString("Description"))
-
             image.add(userDetail.getString("Thumb"))
 
         }
+    }
+    private fun close(){
+        retete.clear()
+        images.clear()
+        ingrediente.clear()
+        pasi.clear()
     }
     private fun transfretete(fileName: String, name:String){
         // get JSONObject from JSON file
@@ -67,27 +78,34 @@ class CategoriesFragment : Fragment() {
         for (i in 0 until userArray.length()) {
             // create a JSONObject for fetching single color data
             val userDetail = userArray.getJSONObject(i)
-            // fetch color name and category and store it in arraylist
             retete.add(userDetail.getString("Nume"))
             images.add(userDetail.getString("Thumb"))
-            ingrediente.add(userDetail.getString("In1"))
-            ingrediente.add(userDetail.getString("In2"))
-            ingrediente.add(userDetail.getString("In3"))
+            ingrediente.add(userDetail.getString("In"))
+            pasi.add(userDetail.getString("P"))
         }
     }
 
     private fun loadJSONFromAsset(fileName: String): String {
-        var fileText =""
         val am = requireActivity().assets
         val inputStream: InputStream = am.open(fileName)
         val size: Int = inputStream.available()
         val buffer = ByteArray(size)
         inputStream.read(buffer)
-        fileText = String(buffer)
+        return String(buffer)
 
-        return fileText
     }
+    private val reteteAdapter=ReteteAdapter(retete,ingrediente,images,pasi)
+    private fun setuprvRetete(){
+        binding.rvRetete.apply {
+            layoutManager = LinearLayoutManager(
+                requireActivity(),
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+            adapter = reteteAdapter
+        }
 
+    }
     private fun setupRv() {
 
         binding.rvCategories.apply {
@@ -99,21 +117,20 @@ class CategoriesFragment : Fragment() {
             override fun onCategoryClick(category: String) {
                 when (category) {
                     ("Vita")->{ rv_categories.isVisible=false
-                        binding.rvRetete.apply {
-                        layoutManager = LinearLayoutManager(
-                            requireActivity(),
-                            LinearLayoutManager.VERTICAL,
-                            false
-                        )
-                        adapter = vitaAdapter
+                        close()
+                        transfretete(VITA,"vita")
+                        setuprvRetete()
                     }
+                    ("Pui")->{ rv_categories.isVisible=false
+                        close()
+                        transfretete(PUI,"pui")
+                       setuprvRetete()
                     }
-                    ("Pui")->{  binding.rvCategories.apply {
-                        layoutManager = GridLayoutManager(
-                            requireActivity(),
-                            3)
-                        adapter = categoriesAdapter
-                    }}
+                    ("Mic dejun")->{ rv_categories.isVisible=false
+                        close()
+                        transfretete(DEJUN,"dejun")
+                        setuprvRetete()
+                    }
                     else->null
                 }
             }
@@ -125,5 +142,7 @@ class CategoriesFragment : Fragment() {
     companion object {
         const val CATEGORIES = "categories.json"
         const val VITA="vita.json"
+        const val PUI="pui.json"
+        const val DEJUN="dejun.json"
     }
 }
--- app/src/main/java/com/example/licentaincercarea1/ReteteAdapter.kt
+++ app/src/main/java/com/example/licentaincercarea1/ReteteAdapter.kt
@@ -1,28 +1,32 @@
 package com.example.licentaincercarea1
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.licentaincercarea1.databinding.VitaItemBinding
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils.fitCenter
+import com.example.licentaincercarea1.databinding.ReteteItemBinding
+import java.text.FieldPosition
 
 class ReteteAdapter(val retetele : ArrayList<String>,
                     val ingrediente : ArrayList<String>,
                     val images: ArrayList<String>,
                     val pasi:ArrayList<String>) :
-    RecyclerView.Adapter<ReteteAdapter.VitaViewHolder>() {
-    var listener: vitaClickListener? = null
+    RecyclerView.Adapter<ReteteAdapter.ReteteViewHolder>() {
+    var listener: reteteClickListener? = null
 
-    fun setCategoryClickListener(vitaListener: ReteteAdapter.vitaClickListener) {
-        listener = vitaListener
+    fun setItemClickListener(reteteListener: ReteteAdapter.reteteClickListener) {
+        listener = reteteListener
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-            VitaViewHolder {
-        val binding = VitaItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return VitaViewHolder(binding)
+            ReteteViewHolder {
+        val binding = ReteteItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ReteteViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ReteteAdapter.VitaViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ReteteAdapter.ReteteViewHolder, position: Int) {
         holder.bind()
     }
 
@@ -30,25 +34,34 @@ class ReteteAdapter(val retetele : ArrayList<String>,
         return retetele.size
     }
 
-    inner class VitaViewHolder(private val binding: VitaItemBinding) :
+    inner class ReteteViewHolder(private val binding: ReteteItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind() {
             val retete = retetele[adapterPosition]
-            val images = retetele[adapterPosition]
+            val ing=ingrediente[adapterPosition]
+            val images = images[adapterPosition]
+            val gatit=pasi[adapterPosition]
             binding.name.text = retete
             Glide.with(binding.root.context).load(images).fitCenter().into(binding.imagine)
             setOnItemClickListener()
         }
-
         private fun setOnItemClickListener() {
             binding.root.setOnClickListener {
-                listener?.onCategoryClick(retetele[adapterPosition])
+                val bundle = Bundle().apply {
+                    putString("nume", retetele[adapterPosition])
+                    putString("ingrediente",ingrediente[adapterPosition])
+                    putString("imagine",images[adapterPosition])
+                    putString("pasi", pasi[adapterPosition])
+                }
+                it.findNavController().navigate(R.id.action_categoriesFragment_to_reteteFragment, bundle)
+                //listener?.onCategoryClick(retetele[adapterPosition],ingrediente[adapterPosition],
+                //images[adapterPosition],pasi[adapterPosition])
 
             }
         }
     }
-    interface vitaClickListener{
-        fun onCategoryClick(retete: String)
+    interface reteteClickListener{
+     //   fun onCategoryClick(retete:String, ing:String,images:String,gatit:String )
     }
 
 }
