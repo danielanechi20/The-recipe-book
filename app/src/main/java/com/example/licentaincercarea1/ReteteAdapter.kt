c1d22bb09f280077910e34a2722740a5f7c03d77
danielanechi20 <daniela_nechifor33@yahoo.com>
2023-04-01 16:18:07 +0300

    varianta perfecta <3

--- app/src/main/java/com/example/licentaincercarea1/ReteteAdapter.kt
+++ app/src/main/java/com/example/licentaincercarea1/ReteteAdapter.kt
@@ -7,15 +7,13 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.TransformationUtils.fitCenter
+import com.example.licentaincercarea1.data.category
+import com.example.licentaincercarea1.data.reteta
 import com.example.licentaincercarea1.databinding.ReteteItemBinding
 import java.text.FieldPosition
 
-class ReteteAdapter(val retetele : ArrayList<String>,
-                    val ingrediente : ArrayList<String>,
-                    val images: ArrayList<String>,
-                    val pasi:ArrayList<String>) :
+class ReteteAdapter(val retete:List<reteta>) :
     RecyclerView.Adapter<ReteteAdapter.ReteteViewHolder>() {
-    private var isFavourite: BooleanArray = BooleanArray(retetele.size)
     //var listener: ReteteViewHolder.reteteClickListener? = null
 
     //fun setItemClickListener(reteteListener: ReteteAdapter.reteteClickListener) {
@@ -32,30 +30,25 @@ class ReteteAdapter(val retetele : ArrayList<String>,
     }
 
     override fun getItemCount(): Int {
-        return retetele.size
+        return retete.size
     }
 
     inner class ReteteViewHolder(private val binding: ReteteItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind() {
-            val retete = retetele[adapterPosition]
-            val ing=ingrediente[adapterPosition]
-            val images = images[adapterPosition]
-            val gatit=pasi[adapterPosition]
-            binding.name.text = retete
-            Glide.with(binding.root.context).load(images).fitCenter().into(binding.imagine)
-            binding.favourite.setOnClickListener {
-                isFavourite[adapterPosition]=binding.favourite.isChecked
-            }
+            val reteta=retete[adapterPosition]
+            binding.name.text=reteta.Nume
+            Glide.with(binding.root.context).load(reteta.Thumb).fitCenter().into(binding.imagine)
             setOnItemClickListener()
         }
         private fun setOnItemClickListener() {
+            val reteta=retete[adapterPosition]
             binding.root.setOnClickListener {
                 val bundle = Bundle().apply {
-                    putString("nume", retetele[adapterPosition])
-                    putString("ingrediente",ingrediente[adapterPosition])
-                    putString("imagine",images[adapterPosition])
-                    putString("pasi", pasi[adapterPosition])
+                    putString("nume", reteta.Nume)
+                    putString("ingrediente",reteta.In)
+                    putString("imagine",reteta.Thumb)
+                    putString("pasi", reteta.P)
                 }
                 it.findNavController().navigate(R.id.action_categoriesFragment_to_reteteFragment, bundle)
                 //listener?.onCategoryClick(retetele[adapterPosition],ingrediente[adapterPosition],
--- app/src/main/java/com/example/licentaincercarea1/data/reteta.kt
+++ app/src/main/java/com/example/licentaincercarea1/data/reteta.kt
@@ -12,25 +12,9 @@ data class reteta (
     var Nume:String="",
     @SerializedName(value="Thumb")
     var Thumb:String="",
-    @SerializedName(value="In1")
-    var In1:String="",
-    @SerializedName(value="In2")
-    var In2:String="",
-    @SerializedName(value="In3")
-    var In3:String="",
-    @SerializedName(value="P1")
-    var P1:String="",
-    @SerializedName(value="P2")
-    var P2:String="",
-    @SerializedName(value="P3")
-    var P3:String="",
-    @SerializedName(value="P4")
-    var P4:String="",
-    @SerializedName(value="P5")
-    var P5:String="",
-    @SerializedName(value="P6")
-    var P6:String="",
-    @SerializedName(value="P7")
-    var P7:String="",
+    @SerializedName(value="In")
+    var In:String="",
+    @SerializedName(value="P")
+    var P:String=""
         ):Parcelable
 
